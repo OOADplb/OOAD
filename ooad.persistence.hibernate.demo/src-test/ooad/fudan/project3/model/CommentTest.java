33c60f4c8583a7710a6281fbb2c545d219ab508f
liliumao <371812917@qq.com>
2015-01-17 01:07:31 +0800

    CommentTest change

--- ooad.persistence.hibernate.demo/src-test/ooad/fudan/project3/model/CommentTest.java
+++ ooad.persistence.hibernate.demo/src-test/ooad/fudan/project3/model/CommentTest.java
@@ -9,7 +9,7 @@ public class CommentTest extends HibernateBaseTest {
 	@Test
 	public void test() {
 		PaperBook paperBook = PaperBook.create(null, "pb1", getPersistenceManager());
-		Comment c = Comment.create(paperBook, "c1", getPersistenceManager());
+		Comment c = Comment.create(paperBook, "c1", "uri", getPersistenceManager());
 		c.setURI("127.0.0.1");
 		
 		String hql1 = "from Comment c1 where c1.book.title='pb1' and c1.URI='127.0.0.1'";
